2cff9f38639863715905ca0b0cc542631b078994
Edson733 <20213tn034@utez.edu.mx>
2026-01-13 14:52:10 -0600

    Mostrar resultados de busqueda

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -4,6 +4,7 @@ import { environment } from '@environments/environment';
 import type { GiphyResponse } from '../interfaces/giphy.interfaces';
 import { Gif } from '../interfaces/gif.interfaces';
 import { GifMapper } from '../mapper/gif.mapper';
+import { map } from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class GifsService {
@@ -29,15 +30,21 @@ export class GifsService {
     }
 
     searchGifs(query: string) {
-        this.http.get<GiphyResponse>(`${environment.giphyUrl}/gifs/search`, {
+        return this.http.get<GiphyResponse>(`${environment.giphyUrl}/gifs/search`, {
             params: {
                 api_key: environment.giphyApikey,
                 q: query,
                 limit: 25,
             }
-        }).subscribe( (response) => {
-            const gifs = GifMapper.mapGiphyItemsToGifArray(response.data);
-            console.log(gifs);
-        });
+        }).pipe(
+            map(({data}) => data),
+            map((items) => GifMapper.mapGiphyItemsToGifArray(items))
+
+            // TODO: Historial
+        );
+        // .subscribe( (response) => {
+        //     const gifs = GifMapper.mapGiphyItemsToGifArray(response.data);
+        //     console.log(gifs);
+        // });
     }
 }
